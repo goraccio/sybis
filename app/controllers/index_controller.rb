a2048aeac3d13af887d15b142a3c29100ddb6c52
goraccio <t2nkist@gmail.com>
2017-02-03 16:15:03 +0700

    (+) Product report xls generator w/o filters

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -27,4 +27,21 @@ class IndexController < ApplicationController
     book.write new_file
     send_file new_file
   end
+
+  def generate_product_report
+    require 'spreadsheet'
+
+    @products = Sale.all.group_by{ |p| p.product}
+
+    book = Spreadsheet::Workbook.new
+    sheet1 = book.create_worksheet name: 'Лист 1'
+    sheet1.row(0).concat %w{Товар Сумма}
+    @products.each_with_index do |(product, sales), i|
+      sheet1[i+1, 0] = product.name
+      sheet1[i+1, 1] = total_sales(sales)
+    end
+    new_file = "#{Rails.root}/public/product_reports/product-report-#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.xls"
+    book.write new_file
+    send_file new_file
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'monthly', controller: 'index', action: 'monthly_report'
   get 'total', controller: 'index', action: 'product_cost_report'
   post 'generatemr', controller: 'index', action: 'generate_monthly_report'
+  post 'generatepr', controller: 'index', action: 'generate_product_report'
 end
